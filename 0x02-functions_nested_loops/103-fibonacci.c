7ad95a4c16b0a090c0b4b206c6bb5d8c1cc39208
DejiP <badakindeji@gmail.com>
2023-03-21 15:50:36 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,71 +1,34 @@
-#include <iostream>
-using namespace std;
+#include"main.h"
 
-int sum(int n);
-int fib(int n);
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sum of even-valued
+ *		Fibonacci sequence not exceed
+ *		4million
+ *
+ * Return: Always 0 (Success)
+*/
 
-const int N = 4000000;
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
 
-int main() {
+	while (1)
+	{
+		sum = fib1 + fib2;
 
-  int i = 2;
+		if (sum > 4000000)
+			break;
 
-	cout << sum(i);
+		if ((sum % 2) == 0)
+			total_sum += sum;
 
-	return 0;
-}
-
-int sum(int n) {
-
-	int sum = 0; // suma de los terminos pares
-
-	while (fib(n) < N) {
-
-		if (fib(n)%2==0) {
-
-			sum += fib(n);
-		}
-
-		n++;
+		fib1 = fib2;
+		fib2 = sum;
 	}
+	printf("%.0f\n", total_sum);
 
-	return sum;
+	return (0);
 }
-
-int fib(int n) {
-
-	int a = 0; // anterior
-	int b = 1; // posterior
-	int c = 0; // suma de a+b
-	int i = 1; // contador
-
-	while (i <= n) {
-
-		c = a + b;
-		a = b;
-		b = c;
-
-		i++;
-	}
-
-	return c;
-}
-shivamsince0502 commented on Aug 31, 2020
-thanks
-
-Jum-eng commented on Aug 29, 2022
-Perfect Sir
-
-bhagi1007 commented on Jan 11
-t = int(input()) # number of test cases
-
-for i in range(t):
-n = int(input())
-a = 1
-b = 2
-res = 0
-while b <= n:
-if b % 2 == 0:
-res += b
-a, b = b, a + b
-print(res)
