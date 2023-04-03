e29855d0029e9db6f5b9ef8e9faf6cd232befda8
DejiP <badakindeji@gmail.com>
2023-04-03 16:55:03 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_chessboard - prints chess
+ *
+ * @a: input
+ *
+ * Return: 0
+*/
+
+void print_chessboard(char (*a)[8])
+{
+int i = 0;
+int j = 0;
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+
+		_putchar(a[i / 8][i - j]);
+		i++;
+	}
+	_putchar('\n');
+}
