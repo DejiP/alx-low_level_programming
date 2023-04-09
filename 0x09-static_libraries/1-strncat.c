c391ee67599877f6f335bb5c546eb1b315c833c1
DejiP <badakindeji@gmail.com>
2023-04-09 16:47:37 -0700

    update

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+* _strncat - concatenate two strings
+* using at most n bytes from src
+* @dest: input the value
+* @src: input the value
+* @n: input the value
+*
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int a;
+ int b;
+ 
+while (dest[a] != '\0')
+{
+a++;
+}
+b = 0;
+while (b < n && src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	_puts("\"At the end, my goal is to be able to create life aiding programs");
+	return (0);
+}
