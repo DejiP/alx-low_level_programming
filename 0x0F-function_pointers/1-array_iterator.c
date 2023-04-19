8ca43a79d3771b23a160f2a0df715aa16f55ed08
DejiP <badakindeji@gmail.com>
2023-04-19 13:14:29 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,8 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i = 0;
-for (; i < size; i++
+unsigned int i = 0;
+ if (array != && size > 0 && action != NULL)
+ for (; i < size; i++
 	action(array[i]);
 }
